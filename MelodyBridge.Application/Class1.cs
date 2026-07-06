e04088d961124cc03deba7a90a3a887ecaba6a3a
WaTh <info@wath.dev>
2026-07-06 23:43:07 +0200

    chore: remove stub Class1.cs and unused SquidWtfDownloader
    Class1.cs was a default template file that served no purpose after the
    application services were refactored into dedicated files. The old
    SquidWtfDownloader has been superseded by the provider-based download
    architecture and is no longer referenced anywhere.

--- MelodyBridge.Application/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MelodyBridge.Application;
-// contains business/services logic
-// gets referenced by UI & Infrastructure
-//! Like the workflow of downloading a track (trying multiple plugins, etc)
-public class Class1
-{
-
-}
