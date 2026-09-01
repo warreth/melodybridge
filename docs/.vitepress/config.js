ab40236be3996238eef23fa4ae12f5c8329e5efe
WaTh <info@wath.dev>
2026-09-01 12:57:32 +0200

    document the oauth login options and why sp_dc stays out

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -5,6 +5,7 @@ export default {
     sidebar: [
       { text: 'Home', link: '/' },
       { text: 'Docker', link: '/docker' },
+      { text: 'Accounts & OAuth', link: '/accounts-and-oauth' },
       { text: 'Photino', link: '/photino' },
       { text: 'Developer', link: '/developer' }
     ]
